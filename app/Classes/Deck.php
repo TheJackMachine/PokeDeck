031317ed0ea039c5fb91740c6aa59cb0bbadbac1
JackMachine <contact@jjd-multimedia.com>
2022-04-05 23:31:30 -0400

    Deck Class + InvalidType Exception definition

---
+++ app/Classes/Deck.php
@@ -0,0 +1,198 @@
+<?php namespace App\Classes;
+
+use Pokemon\Pokemon;
+use App\Exceptions\InvalidType;
+
+class Deck
+{
+
+    protected static $validTypes;
+    protected array $cards = [];
+
+    protected string $typeFocused; // What type of Pokemon this deck is focused ?
+    protected int $typeMaxRange = 16; // Min cards of that type
+    protected int $typeMinRange = 12; // Max cards of that type
+    protected int $energyNumber = 10; // Number of energy card to add
+
+    protected int $deckSize = 60; // Number of cards in the deck
+
+    /**
+     * @throws InvalidType
+     */
+    public function __construct($type)
+    {
+        self::initValidTypes();
+        $this->create($type);
+    }
+
+    /**
+     * Fill this deck
+     * @throws InvalidType
+     */
+    protected function create($type)
+    {
+        // 0 - verify type is valid
+        self::validateType($type);
+        $this->typeFocused = $type;
+
+        // 1 - Add 12 - 16 pokemon card of specific type
+        $this->cards = $this->addPokemonCards($this->typeFocused, $this->numberOfPokemonCards());
+
+        // 2 - Add 10 energy cards
+        $this->cards = [...$this->cards, ...$this->addEnergyCards($this->typeFocused, $this->energyNumber)];
+
+        // 3 - Add training card
+        $this->cards = [...$this->cards, ...$this->addTrainerCards($this->getRemainderCardsNumber())];
+
+    }
+
+    /**
+     * Shuffle the deck
+     */
+    public function shuffle()
+    {
+        // TODO: LMFAO Everyday I'm shuffling
+    }
+
+    /**
+     * return number of pokemon cards to get
+     * @return int
+     */
+    protected function numberOfPokemonCards(): int
+    {
+        return rand($this->typeMaxRange, $this->typeMinRange);
+    }
+
+    /**
+     * Return number of cards to complete the deck
+     */
+    public function getRemainderCardsNumber()
+    {
+        return $this->deckSize - count($this->cards);
+    }
+
+    /**
+     * Add 12 - 16 pokemon card of specific type
+     */
+    protected function addPokemonCards($type, $number)
+    {
+        // 1 - Get pokemon of that type
+        $pokemonCards = self::fetchPokemonType($type);
+        $cards = [];
+        // 1a - add to this deck
+        for ($n = 0; $n < $number; $n++) {
+            $cards[] = self::randomCard($pokemonCards);
+        }
+        return $cards;
+    }
+
+    /**
+     * Add 10 energy cards of specific type
+     */
+    protected function addEnergyCards($type, $number): array
+    {
+        $energyCards = self::fetchEnergyType($type);
+        $cards = [];
+        for ($n = 0; $n < $number; $n++) {
+            $cards[] = self::randomCard($energyCards);
+        }
+        return $cards;
+    }
+
+    /**
+     * Add training card
+     */
+    protected function addTrainerCards($number)
+    {
+        $trainerCards = self::fetchTrainingCard();
+        $cards = [];
+        for ($n = 0; $n < $number; $n++) {
+            $cards[] = self::randomCard($trainerCards);
+        }
+        return $cards;
+    }
+
+    public function show()
+    {
+        foreach ($this->cards as $card) {
+            echo $card->getName() . "\r\n";
+        }
+    }
+
+    /**
+     * Return a random element in card array
+     * @param $cards
+     * @return mixed
+     */
+    protected static function randomCard($cards)
+    {
+        return $cards[rand(0, count($cards) - 1)];
+    }
+
+    /**
+     * Fetch Pokemon card by type via API
+     * @param $type
+     * @return array
+     */
+    protected static function fetchPokemonType($type): array
+    {
+        return Pokemon::Card()->where([
+            'supertype' => 'Pokémon',
+            'types' => $type
+        ])->all();
+    }
+
+    /**
+     * Fetch Energy card by type via API
+     * @param $type
+     * @return array
+     */
+    protected static function fetchEnergyType($type): array
+    {
+        return Pokemon::Card()->where([
+            'supertype' => 'Energy',
+            'name' => $type
+        ])->all();
+    }
+
+    /**
+     * Fetch Training card by type via API
+     * @return array
+     */
+    protected static function fetchTrainingCard(): array
+    {
+        return Pokemon::Card()->where([
+            'supertype' => 'Trainer',
+        ])->all();
+    }
+
+    /**
+     * Hydrate validTypes if needed
+     */
+    protected static function initValidTypes()
+    {
+        if (empty(self::$validTypes)) {
+            self::$validTypes = Pokemon::Type()->all();
+        }
+    }
+
+    public static function setValidTypes(array $types = [])
+    {
+        self::$validTypes = $types;
+    }
+
+    public static function getValidTypes()
+    {
+        return self::$validTypes;
+    }
+
+    /**
+     * Check if type is valid
+     * @param $type
+     * @throws InvalidType
+     */
+    protected static function validateType($type)
+    {
+        if (!in_array($type, self::$validTypes)) throw new InvalidType();
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,10 @@
 <?php
 
+use Pokemon\Pokemon;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+
+use App\Classes\Deck;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,4 +19,22 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+Route::get('/test', function (Request $request) {
+
+    $deck = new Deck('Fire');
+    $deck->show();
+    dd('stop');
+
+    // $cards = Pokemon::Card()->all();
+    $card = Pokemon::Card()->find('xy1-1');
+    $supertypes = Pokemon::Supertype()->all();
+    $types = Pokemon::Type()->all();
+    $cards = Pokemon::Card()->where([
+        'supertype' => 'Trainer',
+    ])->all();
+
+    dd($supertypes,$types,$card,$cards[100]);
+    return Response::json(['test'=>'top']);
 });
